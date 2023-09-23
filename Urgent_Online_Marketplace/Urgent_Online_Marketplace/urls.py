9aecaf22226475232ac30d28ebc5af2bfbffc76a
Md Golam Mostofa <golammostofa10001@gmail.com>
2023-09-23 19:07:11 +0600

    details.html page design and items details sowing

--- Urgent_Online_Marketplace/Urgent_Online_Marketplace/urls.py
+++ Urgent_Online_Marketplace/Urgent_Online_Marketplace/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('items/', include('items.urls')),
     path('', include('core.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
