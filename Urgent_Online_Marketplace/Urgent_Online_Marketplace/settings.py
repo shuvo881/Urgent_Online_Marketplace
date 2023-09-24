b14836440a6477360c4872968b6acfc2f94edb7d
Md Golam Mostofa <golammostofa10001@gmail.com>
2023-09-25 01:51:40 +0600

    railway deployment

--- Urgent_Online_Marketplace/Urgent_Online_Marketplace/settings.py
+++ Urgent_Online_Marketplace/Urgent_Online_Marketplace/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os.path
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,9 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Urgent_Online_Marketplace/Urgent_Online_Marketplace/wsgi.py
+++ Urgent_Online_Marketplace/Urgent_Online_Marketplace/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Urgent_Online_Marketplace.setti
 
 application = get_wsgi_application()
 
-app = application()
+app = application
