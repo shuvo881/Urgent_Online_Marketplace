3d6d56470b103c06b9f1738450e41aad4306207f
Md Golam Mostofa <golammostofa10001@gmail.com>
2023-09-25 10:12:25 +0600

    railway deployment

--- Urgent_Online_Marketplace/Urgent_Online_Marketplace/settings.py
+++ Urgent_Online_Marketplace/Urgent_Online_Marketplace/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, STATIC_URL)
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATICFILES_DIR = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
